830fcefdd67c25d9a975ea49d8674ea8c18b5214
Ben Parker <bpar164@aucklanduni.ac.nz>
2021-01-13 16:26:14 +1300

    Creating all Assessment types.

--- Dividni/Services/AssessmentService.cs
+++ Dividni/Services/AssessmentService.cs
@@ -23,7 +23,7 @@ namespace Dividni.Services
         public Boolean createAssessment(Assessment assessment, DownloadRequest downloadRequest)
         {
             //Create a subfolder called Assessments (may exist already)
-            var folderPath = Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().Length-7) + "Assessments"; //Replace 'Dividni' with 'Assessments'
+            var folderPath = Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().Length - 7) + "Assessments"; //Replace 'Dividni' with 'Assessments'
             System.IO.Directory.CreateDirectory(folderPath);
             var assessmentPath = folderPath + "\\" + assessment.Name;
             //Create a folder specifically for this assessment
@@ -52,7 +52,6 @@ namespace Dividni.Services
                         var questionString = simpleToString(simple, questionNumber);
                         //Save C# string to a file
                         System.IO.File.WriteAllText(assessmentPath + "\\Q" + questionNumber + ".cs", questionString);
-                        Console.WriteLine("Writing question");
                         questionNumber++;
                     }
 
@@ -68,7 +67,6 @@ namespace Dividni.Services
                         questionHTML += "<li class=\"q\"><p class=\"cws_code_q\">Q" + questionNumber + "</p></li>";
                         //Save C# string to a file
                         System.IO.File.WriteAllText(assessmentPath + "\\Q" + questionNumber + ".cs", advanced.Question);
-                        Console.WriteLine("Writing question");
                         questionNumber++;
                     }
                 }
@@ -80,39 +78,53 @@ namespace Dividni.Services
             //Close the HTML string
             questionHTML += "</ol></div>";
             //Run the Dividni commands, based on the assessment type
-            if (downloadRequest.Type == "standard") {
+            if (downloadRequest.Type == "standard")
+            {
                 //Compile all of the questions
                 executeCommand("/c cd .. & cd Assessments\\" + assessment.Name + " & csc -t:library -lib:\"C:\\Program Files\\Dividni.com\\Dividni\" -r:Utilities.Courses.dll -out:QHelper.dll " + questionIds);
-                //CSS string
-                var CSS = "body {font-family: \"Palatino Linotype\", \"Book Antiqua\", Palatino, serif;} ol.qlist > li.q {margin: 2em 0;} li > ol.a {list-style: upper-alpha;} .xyz {padding-top: 1em; padding-bottom: 1em;}";
                 //Create HTML template
-                //Header with name
-                var HTML = "<html><head><meta charset=\"utf-8\"/><title>" + assessment.Name + "</title><style>" + CSS + "</style></head><body>";
+                var templateHTML = "";
                 //Cover page
-                if (assessment.CoverPage != "") {
-                    HTML += "<div id=\"coverPage\">" + assessment.CoverPage + "</div><p style=\"page-break-after: always;\" />";
-                } 
+                if (assessment.CoverPage != "") templateHTML += "<div id=\"coverPage\">" + assessment.CoverPage + "</div><p style=\"page-break-after: always;\" />";
                 //Questions
-                HTML += "<div id=\"questions\">" + questionHTML + "</div>";
+                templateHTML += "<div id=\"questions\">" + questionHTML + "</div>";
                 //Appendix
-                if (assessment.Appendix != "") {
-                    HTML += "<p style=\"page-break-before: always;\"/><div id=\"appendix\">" + assessment.Appendix + "</div></body></html>";
-                } else {
-                    HTML += "</body></html>";
-                }
-                //Create a file for the HTML
-                using (StreamWriter sw = File.CreateText(assessmentPath + "\\Exam.Template.html"))
-                {
-                    sw.WriteLine(HTML);
-                }
+                if (assessment.Appendix != "") templateHTML += "<p style=\"page-break-before: always;\"/><div id=\"appendix\">" + assessment.Appendix + "</div>";
+                //Create the file
+                createHTMLDocument(assessmentPath + "\\Assessment.Template.html", assessment.Name, templateHTML);
                 //Generate assessment
-                Console.WriteLine("/c cd .. & cd Assessments\\" + assessment.Name + " & TestGen -lib QHelper.dll -htmlFolder papers -answerFolder answers -paperCount " + downloadRequest.Versions + " Exam.Template.html");
-                executeCommand("/c cd .. & cd Assessments\\" + assessment.Name + " & TestGen -lib QHelper.dll -htmlFolder papers -answerFolder answers -paperCount " + downloadRequest.Versions + " Exam.Template.html");
-                
-                //Convert all html question files in the papers folder to pdf
-                //continueLoop = await this.convertFilesToPDF(`../` + exam.name + `/papers`, exam.name, fileList, path);
+                executeCommand("/c cd .. & cd Assessments\\" + assessment.Name + " & TestGen -lib QHelper.dll -htmlFolder papers -answerFolder answers -paperCount " + downloadRequest.Versions + " Assessment.Template.html");
             }
-
+            else
+            { //LMS Assessment
+                //Cover Page and Appendix
+                if (assessment.CoverPage != "")
+                {
+                    createHTMLDocument(assessmentPath + "\\Assessment.CoverPage.html", assessment.Name + " Cover Page", assessment.CoverPage);
+                    questionIds = "Assessment.CoverPage.html " + questionIds;
+                }
+                if (assessment.Appendix != "")
+                {
+                    createHTMLDocument(assessmentPath + "\\Assessment.Appendix.html", assessment.Name + " Appendix", assessment.Appendix);
+                    questionIds += "Assessment.Appendix.html";
+                }
+                if (downloadRequest.Type == "moodle")
+                {
+                    executeCommand("/c cd .. & cd Assessments\\" + assessment.Name + " & MoodleGen -variants " + downloadRequest.Versions + " -xmlFolder questions -bank " + assessment.Name + " " + questionIds);
+                }
+                else
+                {
+                    //Determine qtiVers
+                    var qtiVers = "";
+                    if (downloadRequest.Type == "canvas") {
+                        qtiVers = "1.2";
+                    } else {
+                        qtiVers = "2.1";
+                    }
+                    //Create LMS compatible zip 
+                    executeCommand("/c cd .. & cd Assessments\\" + assessment.Name + " & QtiGen -qtiVersion " + qtiVers + " -variants " + downloadRequest.Versions + " -id " + assessment.Name + " " + questionIds);
+                }
+            }
             //Delete the Assessments folder and any subdirectories
             //System.IO.Directory.Delete(folderPath, true);
             return true;
@@ -132,7 +144,7 @@ namespace Dividni.Services
         {
             //Build the C# file from the question details
             var questionString = "using System;namespace Utilities.Courses {public partial class QHelper : IQHelper";
-            questionString += "{public static string Q" + questionNumber + "(Random random, Action<string, ushort> registerAnswer, bool isProof)"; 
+            questionString += "{public static string Q" + questionNumber + "(Random random, Action<string, ushort> registerAnswer, bool isProof)";
             questionString += "{var q = Quest_Q" + questionNumber + "(random, isProof);string rval = q.GetQuestion(registerAnswer);return rval;}";
             questionString += "public static QuestionBase Quest_Q" + questionNumber + "(Random random, bool isProof) { var q = new " + simple.Type + "Question(random, isProof);";
             questionString += "q.Id = \"Q" + questionNumber + "\";";
@@ -141,37 +153,60 @@ namespace Dividni.Services
             //Correct answers
             questionString += "q.AddCorrects(";
             var correctAnswers = JsonSerializer.Deserialize<string[]>(simple.CorrectAnswers);
-                for (int i = 0; i < correctAnswers.Length; i++)
-                {
-                    if (i == (correctAnswers.Length-1)) { //Close list with last answer
-                        questionString += "@\"" + correctAnswers[i] + "\");";
-                    } else {
-                        questionString += "@\"" + correctAnswers[i] + "\",";
-                    }  
+            for (int i = 0; i < correctAnswers.Length; i++)
+            {
+                if (i == (correctAnswers.Length - 1))
+                { //Close list with last answer
+                    questionString += "@\"" + correctAnswers[i] + "\");";
                 }
+                else
+                {
+                    questionString += "@\"" + correctAnswers[i] + "\",";
+                }
+            }
             //Incorrect answers
             questionString += "q.AddIncorrects(";
             var incorrectAnswers = JsonSerializer.Deserialize<string[]>(simple.IncorrectAnswers);
-                for (int i = 0; i < incorrectAnswers.Length; i++)
-                {
-                    if (i == (incorrectAnswers.Length-1)) { //Close list with last answer
-                        questionString += "@\"" + incorrectAnswers[i] + "\");";
-                    } else {
-                        questionString += "@\"" + incorrectAnswers[i] + "\",";
-                    }     
+            for (int i = 0; i < incorrectAnswers.Length; i++)
+            {
+                if (i == (incorrectAnswers.Length - 1))
+                { //Close list with last answer
+                    questionString += "@\"" + incorrectAnswers[i] + "\");";
                 }
+                else
+                {
+                    questionString += "@\"" + incorrectAnswers[i] + "\",";
+                }
+            }
             questionString += "return q; }}}";
             return questionString;
         }
 
-        public void executeCommand(string command) {
+        public void executeCommand(string command)
+        {
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "cmd.exe";
             startInfo.Arguments = command;
             process.StartInfo = startInfo;
-            process.Start(); 
+            process.Start();
+            process.WaitForExit();
+        }
+
+        //Creates HTML document from a given string
+        public void createHTMLDocument(string path, string title, string content)
+        {
+            //CSS string
+            var CSS = "body {font-family: \"Palatino Linotype\", \"Book Antiqua\", Palatino, serif;} ol.qlist > li.q {margin: 2em 0;} li > ol.a {list-style: upper-alpha;} .xyz {padding-top: 1em; padding-bottom: 1em;}";
+            //Header with title
+            var HTML = "<html><head><meta charset=\"utf-8\"/><title>" + title + "</title><style>" + CSS + "</style></head><body>";
+            HTML += "<div>" + content + "</div></body></html>";
+            //Create file
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(HTML);
+            }
         }
     }
 }
