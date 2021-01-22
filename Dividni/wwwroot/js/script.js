fb51bfb57ecabbff15a8445fcec623cdd77eac6b
Ben Parker <bpar164@aucklanduni.ac.nz>
2021-01-22 17:29:11 +1300

    Ace Code editor

--- Dividni/wwwroot/js/script.js
+++ Dividni/wwwroot/js/script.js
@@ -18,5 +18,9 @@ $(document).ready(function () {
     $('.modal-not-dismissible').modal({ dismissible: false });
     $('#modal').modal('open');
     $('select').formSelect();
+    //Ace Code Editor
+    let editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/csharp");
 });
 
