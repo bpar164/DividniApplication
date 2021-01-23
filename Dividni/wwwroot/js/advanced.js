40ec5a49f640d5bd78889b3d843b92d61e3e3832
Ben Parker <bpar164@aucklanduni.ac.nz>
2021-01-23 17:49:08 +1300

    Displaying Advanced question code.

--- Dividni/wwwroot/js/advanced.js
+++ Dividni/wwwroot/js/advanced.js
@@ -1,6 +1,13 @@
 let editor;
 
 $(document).ready(() => {
+  //Set-up code editor
+  if (document.getElementById('editor') != null) {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chrome");
+    editor.session.setMode("ace/mode/csharp");
+    document.getElementById('editor').style.fontSize = '14px';
+  }
   //For certain pages
   let location = window.location.href;
   if (location.includes('/Advanced/Create')) {
@@ -18,23 +25,19 @@ $(document).ready(() => {
 });
 
 displayEditorEnabled = (displaySkeleton) => {
-  editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/csharp");
   if (displaySkeleton === true) {
-
-  } else {
-
+    let skeletonCode = `using System;\r\nusing System.Text;\r\n\r\nnamespace Utilities.Courses\r\n{\r\n   public partial class QHelper : IQHelper\r\n   {\r\n      public static string /*QuestionName*/(Random random, Action<string, ushort> registerAnswer, bool isProof) \r\n      {\r\n         var q = new /*Truth or Xyz*/Question(random, isProof); \r\n         q.Id = \"\"; //QuestionId\r\n         q.Marks = 1; //QuestionMarks\r\n         q.Stem = \"\"; //QuestionText\r\n         q.AddCorrects(\r\n            \r\n         ); \r\n         q.AddIncorrects(\r\n   \r\n         ); \r\n         string rval = q.GetQuestion(registerAnswer);\r\n         return rval;\r\n      } \r\n   } \r\n}\r\n`;
+    editor.setValue(skeletonCode, 1);
+  } else { //Display existing question
+    editor.setValue(JSON.parse(document.getElementById('editor').getAttribute('value')), 1);
   }
 }
 
 displayEditorDisabled = () => {
-  editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/csharp");
+  editor.setOption("readOnly", true); //Read only
+  //Display question in the editor
+  editor.setValue(JSON.parse(document.getElementById('editor').getAttribute('value')), 1);
 }
-
-
 
 testAnswer = () => {
   $('#statusModal').modal('open');
@@ -51,8 +54,9 @@ $("#advancedQuestionForm").submit((event) => {
   let content = '';
   let generate = false;
   //Check that question compiles without errors
+
   if (generate == true) {
-    content ='<p>Display errors.</p>';
+    content = '<p>Display errors.</p>';
   } else {
     content = '<p>Saving question...</p>';
     generate = true;
@@ -67,7 +71,7 @@ $("#advancedQuestionForm").submit((event) => {
 generateAdvancedQuestion = () => {
   document.getElementById('aspName').value = document.getElementById('name').value;
   document.getElementById('aspModifiedDate').value = new Date().toISOString().slice(0, 10);
-  document.getElementById('aspQuestion').value = editor.getValue();
+  document.getElementById('aspQuestion').value = JSON.stringify(editor.getValue());
   //Submit the hidden form
   document.getElementById("aspQuestionForm").submit();
 }
